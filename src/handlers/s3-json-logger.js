8e7e2a3c8af8747f943b57cff165db5573ef6e42
harprit <harprits10@gmail.com>
2019-10-27 23:41:09 +0800

    updated for readability

--- src/handlers/s3-json-logger.js
+++ src/handlers/s3-json-logger.js
@@ -1,13 +1,7 @@
-// Create clients outside of the handler
-
-// Create a client to read objects from S3
 const AWS = require('aws-sdk');
 
 const s3 = new AWS.S3();
 
-/**
-  * A Lambda function that logs the payload received from S3.
-  */
 exports.s3JsonLoggerHandler = async (event, context) => {
     const getObjectRequests = event.Records.map(async (record) => {
         const params = {
@@ -16,8 +10,6 @@ exports.s3JsonLoggerHandler = async (event, context) => {
         };
         try {
             const { Body } = await s3.getObject(params).promise();
-            // All log statements are written to CloudWatch by default. For more information, see
-            // https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-logging.html
             console.log(Body.toString());
         } catch (error) {
             console.error('Error calling S3 getObject:', error);
